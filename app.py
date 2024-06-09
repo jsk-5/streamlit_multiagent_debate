f8c81a9554d9403d8b0aeadabab26f4c19a10d79
Joel Edgar <joeledgar5@gmail.com>
2024-06-09 21:40:06 +0100

    Conversational rag completed

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
 import streamlit as st
 import boto3
 
-from botocore.client import Config
 from langchain.chains import RetrievalQA
 from langchain_aws import BedrockLLM
 from langchain_community.retrievers import AmazonKnowledgeBasesRetriever
 import time
+from langchain.chains import create_history_aware_retriever
+from langchain_core.prompts import MessagesPlaceholder
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_core.runnables import Runnable
+from langchain.tools.retriever import create_retriever_tool
+from langgraph.prebuilt import create_react_agent
+from langgraph.checkpoint.sqlite import SqliteSaver
+import logging
 
 
-def response_generator(response):
-    for word in response.split(" "):
-        yield word + " "
-        time.sleep(0.05)
 
 retriever = AmazonKnowledgeBasesRetriever(
     knowledge_base_id="O9O2CCKXV4",
@@ -22,22 +33,79 @@ model_kwargs_claude = {"temperature": 0, "top_k": 10, "max_tokens_to_sample": 30
 
 llm = BedrockLLM(model_id="anthropic.claude-v2", model_kwargs=model_kwargs_claude)
 
-qa = RetrievalQA.from_chain_type(
-    llm=llm, retriever=retriever, return_source_documents=True
+contextualize_q_system_prompt = (
+    "Given a chat history and the latest user question "
+    "which might reference context in the chat history, "
+    "formulate a standalone question which can be understood "
+    "without the chat history. Do NOT answer the question, "
+    "just reformulate it if needed and otherwise return it as is."
 )
+contextualize_q_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", contextualize_q_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+history_aware_retriever = create_history_aware_retriever(
+    llm, retriever, contextualize_q_prompt
+)
+
+
+### Answer question ###
+system_prompt = (
+    "You are an assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer "
+    "the question. If you don't know the answer, say that you "
+    "don't know. Use three sentences maximum and keep the "
+    "answer concise."
+    "\n\n"
+    "{context}"
+)
+qa_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+
+rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+
 
 
 st.title("Rag test")
 
-# Set OpenAI API key from Streamlit secrets
+
 
 # Set a default model
 if "anthropic_model" not in st.session_state:
     st.session_state["anthropic_model"] = "anthropic.claude-v2"
-
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
+
+if "store" not in st.session_state:
+    st.session_state["store"] = {}
+
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in st.session_state["store"]:
+        st.session_state["store"][session_id] = ChatMessageHistory()
+    return st.session_state["store"][session_id]
+
+
+conversational_rag_chain = RunnableWithMessageHistory(
+    rag_chain,
+    get_session_history,
+    input_messages_key="input",
+    history_messages_key="chat_history",
+    output_messages_key="answer",
+)
+
+
+
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
@@ -51,12 +119,15 @@ if prompt := st.chat_input("Ask me a question about my data"):
     # Display user message in chat message container
     with st.chat_message("user"):
         st.markdown(prompt)
-
-
+    response = conversational_rag_chain.invoke(
+        {"input": prompt},
+        config={
+            "configurable": {"session_id": "abc123"}
+        },  # constructs a key "abc123" in `store`.
+    )["answer"]
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
-        response = qa(prompt)['result']
-        st.write_stream(response_generator(response))
-        # st.markdown(response)
-        # print(response)
+        
+        st.markdown(response)
     st.session_state.messages.append({"role": "assistant", "content": response})
+
